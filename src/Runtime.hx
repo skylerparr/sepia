4fa1294e398744e719ebd1bf7ea9d5245e07c333
Skyler Parr <skylerparr@gmail.com>
2018-12-20 13:12:19 -0500

    added complete callback when compilation finishes

--- src/Runtime.hx
+++ src/Runtime.hx
@@ -14,12 +14,61 @@ import sys.FileSystem;
 import sys.io.File;
 class Runtime {
 
-  private static var src: String;
-  private static var output: String;
-  private static var classPaths: Array<String>;
-  private static var libs: Array<String>;
+  @:isVar
+  public static var src(get, set):String;
+  @:isVar
+  public static var output(get, set):String;
+  @:isVar
+  public static var classPaths(get, set):Array<String>;
+  @:isVar
+  public static var libs(get, set):Array<String>;
+  @:isVar
+  public static var defines(get, set): Array<String>;
 
-  public static function main(src: String, output: String, classPaths: Array<String>, libs: Array<String>, onComplete: Void->Void) {
+  private static var completeCallback: Array<String>->Void;
+
+  static function get_src():String {
+    return src;
+  }
+
+  static function set_src(value:String):String {
+    return src = value;
+  }
+
+  static function get_output():String {
+    return output;
+  }
+
+  static function set_output(value:String):String {
+    return output = value;
+  }
+
+  static function get_classPaths():Array<String> {
+    return classPaths;
+  }
+
+  static function set_classPaths(value:Array<String>):Array<String> {
+    return classPaths = value;
+  }
+
+  static function get_libs():Array<String> {
+    return libs;
+  }
+
+  static function set_libs(value:Array<String>):Array<String> {
+    return libs = value;
+  }
+
+  static function get_defines():Array<String> {
+    return defines;
+  }
+
+  static function set_defines(value:Array<String>):Array<String> {
+    return defines = value;
+  }
+
+
+  public static function main(src: String, output: String, classPaths: Array<String>, libs: Array<String>, onComplete: Array<String>->Void) {
     ScriptMacros;
     CppiaObjectFactory;
 
@@ -27,19 +76,21 @@ class Runtime {
     Runtime.output = output;
     Runtime.classPaths = classPaths;
     Runtime.libs = libs;
+    Runtime.completeCallback = onComplete;
 
     var variables = HScriptEval.interp.variables;
 
     variables.set("recompile", recompile);
     variables.set("r", recompile);
     variables.set("Type", Type);
+    variables.set("Reflect", Reflect);
     variables.set("Macro", hscript.Macro);
     variables.set("Parser", hscript.Parser);
     variables.set("Interp", hscript.Interp);
 
     var parser: Parser = new Parser();
     var interp: Interp = new Interp();
-    var pos: Position = {file: "Runtime.hx", min: 0, max: 65535}
+    var pos: Position = {file: null, min: 0, max: 12}
     var hscriptMacro: Macro = new Macro(pos);
 
     variables.set("eval", function(s: String): Dynamic {
@@ -56,12 +107,7 @@ class Runtime {
       return hscriptMacro.convert(ast);
     });
 
-    recompile();
-
-    trace(onComplete);
-    if(onComplete != null) {
-      onComplete();
-    }
+    var files = recompile();
 
     IHx.main();
   }
@@ -72,6 +118,10 @@ class Runtime {
 
     load();
     reloadClasses(files);
+
+    if(completeCallback != null) {
+      completeCallback(files);
+    }
 
     return files;
   }
